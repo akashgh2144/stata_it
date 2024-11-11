982621242e01256fab22310726ec9267274c12f9
akash2144 <ghoshbabu2144@gmail.com>
2024-11-11 18:44:18 +0600

    here added some animated card,image and description

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -6,6 +6,7 @@ import Banner4 from '../Banner4/Banner4';
 import Banner5 from '../Banner5/Banner5';
 import Banner6 from '../Banner6/Banner6';
 import Banner7 from '../Banner7/Banner7';
+import Banner8 from '../Banner8/Banner8';
 
 const Home = () => {
     return (
@@ -17,6 +18,7 @@ const Home = () => {
             <Banner5></Banner5>
             <Banner6></Banner6>
             <Banner7></Banner7>
+            <Banner8></Banner8>
         </div>
     );
 };
